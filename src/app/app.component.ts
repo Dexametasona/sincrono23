975141033afa6f75acd45bad4f26285f9032a1a5
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-24 10:50:21 -0500

    sweet alert para el logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Auth, getAuth, User } from '@angular/fire/auth';
+import { Component, OnInit } from '@angular/core';
 import { AuthguardService } from './service/authguard.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sincrono23';
-  constructor(private auth:AuthguardService){}
-  logout():void{
-    this.auth.logout().then(()=>console.log('sesión cerrada')).catch(err=>console.log('ocurrio un error: '+err))
+  usuario!:string|null
+  constructor(private auth:AuthguardService, private route:Router){}
 
+  logout():void{
+    const auth:Auth=getAuth()
+    const user:User|null=auth.currentUser;
+    if(user!=null){
+      Swal.fire({
+        icon:'warning',
+        title:'Seguro que desea salir sesión',
+        showCancelButton:true,
+        confirmButtonText:'si',
+        cancelButtonColor:'#3085d6',
+        confirmButtonColor:'#d33'
+      }).then((res)=>{
+        if(res){
+          this.auth.logout().then(()=>console.log('sesión cerrada')).catch(err=>console.log('ocurrio un error: '+err))
+          this.route.navigate(['/login'])
+        }
+      })
+    }else{
+      Swal.fire({
+        icon:'info',
+        text:'Usuario no logeado'
+      })
+    }
+    
+    
+
+  }
+  ngOnInit(): void {
+    
   }
 }
